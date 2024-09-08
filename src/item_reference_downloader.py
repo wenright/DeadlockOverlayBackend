1b689481ed0901acaa1dc97a3250742c8e054745
Will Enright <wenright0@gmail.com>
2024-09-08 17:55:30 -0500

    fix test cases

--- src/item_reference_downloader.py
+++ src/item_reference_downloader.py
@@ -7,3 +7,5 @@ while True:
   file_name = "../data/items/" + item_name + ".png"
   stream_frame, resolution = pull_frame("https://www.twitch.tv/mattercomm")
   cropped_image = crop_item(stream_frame, resolution, item_slots_1080p[0], file_name)
+
+  print(item_name + " saved to " + file_name)
--- src/test_item_finder.py
+++ src/test_item_finder.py
@@ -6,8 +6,8 @@ default_resolution = (1920, 1080)
 
 class Test(unittest.TestCase):
   def test_item_finder_simple(self):
-    test_frame_0 = Image.open("../data/testing/test_simple.png")
-    items = item_finder.get_items(test_frame_0, default_resolution)
+    test_frame = Image.open("../data/testing/test_simple.png")
+    items = item_finder.get_items(test_frame, default_resolution)
 
     self.assertEqual(items[0], "Headshot Booster")
     self.assertEqual(items[1], "Warp Stone")
@@ -23,23 +23,23 @@ class Test(unittest.TestCase):
     self.assertEqual(items[2], "Mystic Shot")
     self.assertEqual(items[3], "Empty")
 
-  def test_item_finder_simple(self):
-    test_frame_0 = Image.open("../data/testing/test_on_cooldown_1.png")
-    items = item_finder.get_items(test_frame_0, default_resolution)
-
-    self.assertEqual(items[0], "Basic Magazine")
-    self.assertEqual(items[1], "Point Blank")
-    self.assertEqual(items[2], "Slowing Bullets")
-    self.assertEqual(items[3], "Fleetfoot")
-  
   def test_item_finder_on_cooldown(self):
-    test_frame_empty = Image.open("../data/testing/test_on_cooldown_2.png")
+    test_frame_empty = Image.open("../data/testing/test_on_cooldown_1.png")
     items = item_finder.get_items(test_frame_empty, default_resolution)
 
     self.assertEqual(items[0], "High Velocity")
     self.assertEqual(items[1], "Monster Rounds")
     self.assertEqual(items[2], "Hollow Point")
     self.assertEqual(items[3], "Mystic Shot")
+
+  def test_item_finder_simple(self):
+    test_frame = Image.open("../data/testing/test_on_cooldown_2.png")
+    items = item_finder.get_items(test_frame, default_resolution)
+
+    self.assertEqual(items[0], "Basic Magazine")
+    self.assertEqual(items[1], "Point Blank")
+    self.assertEqual(items[2], "Slowing Bullets")
+    self.assertEqual(items[3], "Fleetfoot")
 
 if __name__ == '__main__':
   unittest.main()
