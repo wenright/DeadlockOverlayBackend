aa8243e7fe5293aaf686c62e7a1ca03977924d87
Will Enright <wenright0@gmail.com>
2024-09-07 19:22:21 -0500

    implement redis caching

--- src/server.py
+++ src/server.py
@@ -5,24 +5,38 @@ import item_finder
 import stream
 import re
 import json
+import os
 
-r = redis.Redis(host='redis-15121.c281.us-east-1-2.ec2.redns.redis-cloud.com', port=15121, db=0)
+r = redis.Redis(
+  host='redis-15121.c281.us-east-1-2.ec2.redns.redis-cloud.com',
+  port=15121,
+  db=0,
+  password=os.environ.get('REDIS_PASSWORD')
+)
 
 app = Flask(__name__)
 
 """
-returns a list of items if it's already been set.
-otherwise read stream, detect items, and return that list of items, as well as set the value in redis
+returns a list of items that the channelName has on screen.
+if the channelName exists in the cache, it will use that (15s TTL). 
+otherwise pull screenshot from stream, detect items, and return that list of items, as well as set the value in redis
 """
-@app.route("/<channelName>")
+@app.route("/<channelName>", methods=['GET'])
 def get_items(channelName):
   username_regex = re.compile(r'^[a-zA-Z0-9_]{4,25}$')
   if not username_regex.match(channelName):
-    return "Invalid channel name"
+    return Response(
+      response="Invalid channel name",
+      status=400
+    )
   
   existing_items = r.get(channelName)
   if existing_items:
-    return existing_items
+    return Response(
+      response=existing_items,
+      status=200,
+      mimetype='application/json'
+    )
 
   try:
     stream_frame, resolution = stream.pull_frame(f'https://www.twitch.tv/{channelName}')
@@ -39,4 +53,7 @@ def get_items(channelName):
   except Exception:
     print(traceback.format_exc())
   
-  return "Failed to retrieve items"
+  return Response(
+    response="Failed to retrieve items",
+    status=500
+  )
