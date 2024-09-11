ac9b0a4314cc9b757bd2f21413f5f8f505f0094c
Will Enright <wenright0@gmail.com>
2024-09-11 13:31:33 -0500

    trying a new nn architecture

--- src/train.py
+++ src/train.py
@@ -13,7 +13,7 @@ data_dir = pathlib.Path("../data/real_items").with_suffix("")
 
 print("Number of images in data set: " + str(len(list(data_dir.glob("*/*.png")))))
 
-batch_size = 32
+batch_size = 8
 img_width = 30
 img_height = 30
 
@@ -44,14 +44,30 @@ num_classes = len(class_names)
 
 model = Sequential([
   layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
+  # First Convolutional Block
+  layers.Conv2D(32, (3, 3), activation='relu'),
+  layers.BatchNormalization(),
+  layers.MaxPooling2D((2, 2)),
+  layers.Dropout(0.25),
+
+  # Second Convolutional Block
+  layers.Conv2D(64, (3, 3), activation='relu'),
+  layers.BatchNormalization(),
+  layers.MaxPooling2D((2, 2)),
+  layers.Dropout(0.25),
+
+  # Third Convolutional Block
+  layers.Conv2D(128, (3, 3), activation='relu'),
+  layers.BatchNormalization(),
+  layers.MaxPooling2D((2, 2)),
+  layers.Dropout(0.25),
+
+  # Flatten the output of the convolutions for the Dense layers
   layers.Flatten(),
-  layers.Dense(128, activation='relu'),
+  
+  # Fully Connected Layers
+  layers.Dense(256, activation='relu'),
+  layers.Dropout(0.25),
   layers.Dense(num_classes)
 ])
 
@@ -62,7 +78,7 @@ model.compile(optimizer='adam',
 model.summary()
 
 # Train
-epochs=25
+epochs=50
 history = model.fit(
   train_ds,
   validation_data=val_ds,
