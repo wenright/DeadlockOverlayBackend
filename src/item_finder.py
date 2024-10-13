99e5ffcaaf13ac9d94c514e2ce0460d019188d50
Will Enright <wenright0@gmail.com>
2024-10-13 11:42:36 -0500

    fix data_puller.py errors when loading model

--- src/item_finder.py
+++ src/item_finder.py
@@ -34,12 +34,6 @@ item_slots_1080p = {
 
 prefix = "data/clean_items/"
 
-# Load Keras model
-model = None
-with open('data/models/item_classifier_model/model.json', 'r') as model_json:
-  model = keras.models.model_from_json(model_json.read())
-class_names = np.load("data/models/class_names.npy")
-
 def get_items(image, resolution, use_nn=False):
   items = {
     "orange": [],
@@ -73,6 +67,13 @@ def crop_item(image, resolution, slot, file_name=None):
   return cropped_image
 
 def match_item_nn(item_image):
+  # Load Keras model
+  model = None
+  # TODO this doesn't work for loading a tensorflowjs model
+  with open('data/models/item_classifier_model/model.json', 'r') as model_json:
+    model = keras.models.model_from_json(model_json.read())
+  class_names = np.load("data/models/class_names.npy")
+
   img_array = tf.keras.utils.img_to_array(item_image)
   img_array = tf.expand_dims(img_array, 0) # Create a batch
   predictions = model.predict(img_array)
