9632c97344bb71a989eeb1f191dccbb7ade1a289
Will Enright <wenright0@gmail.com>
2024-11-30 15:02:43 -0600

    use .env for twitch auth, add new items to clean data

--- src/stream.py
+++ src/stream.py
@@ -3,14 +3,18 @@ import numpy as np
 import streamlink
 from PIL import Image
 import os
+from dotenv import load_dotenv
 
 session = streamlink.Streamlink()
 session.set_option("twitch-disable-ads", True)
 
+load_dotenv()
 if os.environ.get('TWITCH_AUTH'):
   session.set_option("http-headers", {
     "Authorization": f"OAuth {os.environ.get('TWITCH_AUTH')}"
   })
+else:
+  print("WARNING: You may get a preroll ad instead of training data. Create a .env file at the root level with TWITCH_AUTH=[auth code] using the script on the streamlink URL in the README")
 
 def pull_frame(stream_url):
   # Get the stream URL using streamlink
