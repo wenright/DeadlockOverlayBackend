036fa308025f4139f7538126f7ba2337bdb814e5
wenright <wenright0@gmail.com>
2025-05-09 20:14:08 -0500

    add clean images for all the new items

--- src/item_reference_downloader.py
+++ src/item_reference_downloader.py
@@ -1,11 +1,30 @@
 from stream import pull_frame
 from item_finder import item_slots_1080p, crop_item
+from PIL import Image
+import matplotlib.pyplot as plt
 
-print("Hit enter once the stream is caught up to the item image you're trying to load")
-while True:
+plt.ion()
+f, axarr = plt.subplots(1, 1)
+axarr.imshow(Image.open("data/empty.png"))
+
+input("Hit enter once the stream is caught up to the item image you're trying to load...")
+
+stream_frame, resolution = pull_frame("https://www.twitch.tv/mattercomm")
+idx = 0
+
+while idx < len(item_slots_1080p):
+  cropped_image = crop_item(stream_frame, resolution, item_slots_1080p[idx], None)
+  axarr.imshow(cropped_image)
   item_name = input("Enter item name: ")
+
+  if not item_name:
+    print("Empty input, exiting")
+    break
+  
   file_name = "data/clean_items/" + item_name + ".png"
-  stream_frame, resolution = pull_frame("https://www.twitch.tv/deathydl")
-  cropped_image = crop_item(stream_frame, resolution, item_slots_1080p[0], file_name)
+  cropped_image.save(file_name)
 
   print(item_name + " saved to " + file_name)
+  idx += 1
+
+print("Done")
